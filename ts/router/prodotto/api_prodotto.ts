3557e428929ad5fa66f0cd073addd3c43210c002
Fabio Danubbio <ronaldocms@outlook.it>
2022-05-26 10:14:50 +0200

    fix error logic get with method next

--- ts/router/prodotto/api_prodotto.ts
+++ ts/router/prodotto/api_prodotto.ts
@@ -1,6 +1,5 @@
 import express, { NextFunction, Request, Response } from "express";
 var router = express.Router();
-import MySQLProdotto from "../../database/mysql/prodotto/mysql_prodotto";
 import __api__prodotto_deleteFromParams from "./call/api_prodotto_deleteFromParams";
 import __api__prodotto_getAll__ from "./call/api_prodotto_getAll";
 import __api__prodotto_getBy__ from "./call/api_prodotto_getby";
@@ -13,17 +12,9 @@ router.delete("/:id", async (req: Request, res: Response) => __api__prodotto_del
 router.get("/:id",  (req: Request, res: Response) => __api__prodotto__getFromParams__(req, res));
 router.put("/", bodyParser.json(), async (req: Request, res: Response) => __api__prodotto__put(req, res));
 router.post("/", bodyParser.json(), async (req: Request, res: Response) => __api__prodotto__post(req, res));
+router.get("/", async (req: Request, res: Response, next: NextFunction) => __api__prodotto_getAll__(req, res, next));
 router.get("/", async (req : Request, res : Response) => __api__prodotto_getBy__(req, res));
 
-router.get("/", async function (req: Request, res: Response, next: NextFunction) {
-    var connector = new MySQLProdotto();
-    if (Object.keys(req.query).length != 0) {
-      next();
-      return;
-    }
-    __api__prodotto_getAll__(connector, req, res);
-  }
-);
 
 export function paramsIsNotAccettable(query: any): boolean {
   for (const property in query) {
--- ts/router/prodotto/call/api_prodotto_getAll.ts
+++ ts/router/prodotto/call/api_prodotto_getAll.ts
@@ -1,17 +1,24 @@
 import MySQLProdotto from "../../../database/mysql/prodotto/mysql_prodotto";
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 
 async function __api__prodotto_getAll__(
-    connector: MySQLProdotto,
     req: Request,
-    res: Response
+    res: Response,
+    next: NextFunction
   ) {
+    var connector = new MySQLProdotto();
+    if (Object.keys(req.query).length != 0) {
+      next();
+      return;
+    }
+
     try {
       let prodotti = await connector.select();
       res.send(prodotti);
     } catch (error) {
       res.status(400).end("Errore");
     }
+
   }
 
   export default __api__prodotto_getAll__;
